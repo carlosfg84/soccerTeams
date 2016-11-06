8c792f2ed500738e000529d3fab672354d754213
carlosfg84 <CARLOS@carlosfdadosmbp.home>
2016-11-06 11:54:52 -0800

    Initial Commit

---
+++ soccerTeams/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  soccerTeams
+//
+//  Created by Carlos Flores Guardado on 11/6/16.
+//  Copyright © 2016 Carlos Flores Guardado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
